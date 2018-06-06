1be3e934761de51449bf7c30ebdff6332201bcda
E.C <35582713+bwcont@users.noreply.github.com>
2018-06-05 21:36:40 -0700

    Update MicroGASPFigure.R

--- MicroGASPFigure.R
+++ MicroGASPFigure.R
@@ -3,8 +3,8 @@
 ####11111111#####
 
 data1 <- matrix(nrow = 3, ncol = 3)
-data1 <- matrix(nrow = 3, ncol = 3)
-data1 <- matrix(nrow = 3, ncol = 3)
+data2 <- matrix(nrow = 3, ncol = 3)
+data3 <- matrix(nrow = 3, ncol = 3)
 
 data1[,1] <- c(0,2,7)
 data1[,2] <- c(100000,
@@ -17,7 +17,7 @@ data1[,3] <- c(500000000,
 data3[,1] <- c(0,2,7)
 data3[,2] <- c(7000,
                14000,
-               0)
+               1)
 data3[,3] <- c(700000000,
                17000000000,
                14000000000)
@@ -25,7 +25,7 @@ data3[,3] <- c(700000000,
 data2[,1] <- c(0,2,7)
 data2[,2] <- c(10000,
                20000,
-               0)
+               1)
 data2[,3] <- c(700000000,
                3000000000,
                16000000)
@@ -42,12 +42,12 @@ Young.Cells <-data1[,3]
 
 plot(0,type='n', #make empty plot 
      xlim=c(0, 7), 
-     ylim=c(0, 17000000000) 
+     ylim=c(1, 60000000000) 
      , xlab= 'Time (Days)', 
      ylab = 'Cell Count (CFU/mL)', 
      main = 'Condition 1', 
      pch=19, yaxt='n', axes=F, # removed axes
-     cex.lab=1.05) # make empty plot to fill in
+     cex.lab=1.05, log = 'y') # make empty plot to fill in
 
 axis(1)
 axis(2)
@@ -62,7 +62,7 @@ points(Young.Cells ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch =
 
 lines(Young.Cells ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16)
 
-legend("topright", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
+legend("topleft", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
 
 #####22222222####
 
@@ -79,12 +79,12 @@ Young.Cells2 <-data2[,3]
 
 plot(0,type='n', #make empty plot 
      xlim=c(0, 7), 
-     ylim=c(0, 17000000000) 
+     ylim=c(1, 60000000000) 
      , xlab= 'Time (Days)', 
      ylab = 'Cell Count (CFU/mL)', 
      main = 'Condition 2', 
      pch=19, yaxt='n', axes=F, # removed axes
-     cex.lab=1.05) # make empty plot to fill in
+     cex.lab=1.05, log = 'y') # make empty plot to fill in
 
 axis(1)
 axis(2)
@@ -99,7 +99,7 @@ points(Young.Cells2 ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch 
 
 lines(Young.Cells2 ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16)
 
-legend("topright", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
+legend("topleft", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
 
 
 ####3333333#####
@@ -117,12 +117,12 @@ Young.Cells3 <-data3[,3]
 
 plot(0,type='n', #make empty plot 
      xlim=c(0, 7), 
-     ylim=c(0, 17000000000) 
+     ylim=c(1, 60000000000) 
      , xlab= 'Time (Days)', 
      ylab = 'Cell Count (CFU/mL)', 
      main = 'Condition 3', 
      pch=19, yaxt='n', axes=F, # removed axes
-     cex.lab=1.05) # make empty plot to fill in
+     cex.lab=1.05, log = 'y') # make empty plot to fill in
 
 axis(1)
 axis(2)
@@ -137,7 +137,7 @@ points(Young.Cells3 ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch 
 
 lines(Young.Cells3 ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16)
 
-legend("topright", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
+legend("topleft", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
 
 
 
