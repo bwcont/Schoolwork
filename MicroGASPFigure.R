b326c6c2a0aa063cf6a741451c8c7d24172fae14
E.C <35582713+bwcont@users.noreply.github.com>
2018-06-05 21:28:26 -0700

    Update MicroGASPFigure.R

--- MicroGASPFigure.R
+++ MicroGASPFigure.R
@@ -1,22 +1,42 @@
-#########
 
-data1 <- matrix(nrow = 3, ncol = 4)
+
+####11111111#####
+
+data1 <- matrix(nrow = 3, ncol = 3)
+data1 <- matrix(nrow = 3, ncol = 3)
+data1 <- matrix(nrow = 3, ncol = 3)
+
 data1[,1] <- c(0,2,7)
-data1[,2] <- c(500000000,
+data1[,2] <- c(100000,
+               7000000,
+               100000000)
+data1[,3] <- c(500000000,
                3000000000,
                5000000000)
-data1[,3] <- c(700000000,
-               3000000000,
-               16000000)
-data1[,4] <- c(700000000,
+
+data3[,1] <- c(0,2,7)
+data3[,2] <- c(7000,
+               14000,
+               0)
+data3[,3] <- c(700000000,
                17000000000,
                14000000000)
+
+data2[,1] <- c(0,2,7)
+data2[,2] <- c(10000,
+               20000,
+               0)
+data2[,3] <- c(700000000,
+               3000000000,
+               16000000)
+
+
 data1 <- as.data.frame(data1)
 
 Day <- data1[,1]
-Condition.1 <-data1[,2]
-Condition.2 <-data1[,3]
-Condition.3 <-data1[,4]
+Aged.Cells <-data1[,2]
+Young.Cells <-data1[,3]
+
 
 
 
@@ -24,8 +44,8 @@ plot(0,type='n', #make empty plot
      xlim=c(0, 7), 
      ylim=c(0, 17000000000) 
      , xlab= 'Time (Days)', 
-     ylab = 'Cell Count', 
-     main = '  ', 
+     ylab = 'Cell Count (CFU/mL)', 
+     main = 'Condition 1', 
      pch=19, yaxt='n', axes=F, # removed axes
      cex.lab=1.05) # make empty plot to fill in
 
@@ -33,17 +53,95 @@ axis(1)
 axis(2)
 box()
 
-points(Condition.3 ~ Day, col = "green", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+points(Aged.Cells ~ Day, col = "olivedrab4", lwd = 1, cex = 1.2, lty = 1, pch = 16)
 
-lines(Condition.3 ~ Day, col = "green", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+lines(Aged.Cells ~ Day, col = "olivedrab4", lwd = 1, cex = 1.2, lty = 1, pch = 16)
 
-points(Condition.1 ~ Day, col = "red", lwd = 1, cex = 1.2, lty = 1, pch = 16) 
 
-lines(Condition.1 ~ Day, col = "red", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+points(Young.Cells ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16) 
 
-points(Condition.2 ~ Day, col = "blue", lwd = 1, cex = 1.2, lty = 1, pch = 16) 
+lines(Young.Cells ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16)
 
-lines(Condition.2 ~ Day, col = "blue", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+legend("topright", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
+
+#####22222222####
+
+
+
+data2 <- as.data.frame(data2)
+
+Day <- data2[,1]
+Aged.Cells2 <-data2[,2]
+Young.Cells2 <-data2[,3]
+
+
+
+
+plot(0,type='n', #make empty plot 
+     xlim=c(0, 7), 
+     ylim=c(0, 17000000000) 
+     , xlab= 'Time (Days)', 
+     ylab = 'Cell Count (CFU/mL)', 
+     main = 'Condition 2', 
+     pch=19, yaxt='n', axes=F, # removed axes
+     cex.lab=1.05) # make empty plot to fill in
+
+axis(1)
+axis(2)
+box()
+
+points(Aged.Cells2 ~ Day, col = "olivedrab4", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+
+lines(Aged.Cells2 ~ Day, col = "olivedrab4", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+
+
+points(Young.Cells2 ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16) 
+
+lines(Young.Cells2 ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+
+legend("topright", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
+
+
+####3333333#####
+
+
+
+data3 <- as.data.frame(data3)
+
+Day <- data3[,1]
+Aged.Cells3 <-data3[,2]
+Young.Cells3 <-data3[,3]
+
+
+
+
+plot(0,type='n', #make empty plot 
+     xlim=c(0, 7), 
+     ylim=c(0, 17000000000) 
+     , xlab= 'Time (Days)', 
+     ylab = 'Cell Count (CFU/mL)', 
+     main = 'Condition 3', 
+     pch=19, yaxt='n', axes=F, # removed axes
+     cex.lab=1.05) # make empty plot to fill in
+
+axis(1)
+axis(2)
+box()
+
+points(Aged.Cells3 ~ Day, col = "olivedrab4", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+
+lines(Aged.Cells3 ~ Day, col = "olivedrab4", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+
+
+points(Young.Cells3 ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16) 
+
+lines(Young.Cells3 ~ Day, col = "indianred2", lwd = 1, cex = 1.2, lty = 1, pch = 16)
+
+legend("topright", legend=c('UnAged', 'Aged'), fill = c("indianred2","olivedrab4"), cex=0.9)
+
+
+
+
 
 
 
